791282d8f7b486474efccad8a3a73c9570626328
Angelica <Angelica137@users.noreply.github.com>
2022-06-02 19:38:10 +0100

    refactor

--- scripts/reverse_string.py
+++ scripts/reverse_string.py
@@ -1,7 +1,2 @@
 def reverse_string(word: str) -> str:
-    reversed_string = ""
-    index = len(word) - 1
-    while index >= 0:
-        reversed_string += (word[index])
-        index -= 1
-    return reversed_string
+    return word[::-1]
