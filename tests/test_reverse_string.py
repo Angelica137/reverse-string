f0fff60b3b73fe484050a9ab31161056ead7d1a3
Angelica <Angelica137@users.noreply.github.com>
2022-06-02 19:39:01 +0100

    test longer string, green

--- tests/test_reverse_string.py
+++ tests/test_reverse_string.py
@@ -3,3 +3,7 @@ from scripts.reverse_string import reverse_string
 
 def test_reverse_two_char_string():
     assert reverse_string("lo") == "ol"
+
+
+def test_reverse_long_string():
+    assert reverse_string("abcdefg") == "gfedcba"
